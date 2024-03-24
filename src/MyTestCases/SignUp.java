eafe2c14fdc680022b9ebccee69d392701920361
maria99m <mariamabdulbaki9@gmail.com>
2024-03-25 00:54:04 +0200

    the first four test cases

--- src/MyTestCases/SignUp.java
+++ src/MyTestCases/SignUp.java
@@ -23,7 +23,7 @@ public class SignUp extends parameters {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 	}
 	   
-	@Test()
+	@Test(priority = 1, enabled = true)
 	public void myFirstTest() throws InterruptedException {
 		
 		driver.get("https://magento.softwaretestingboard.com/");
@@ -56,6 +56,63 @@ public class SignUp extends parameters {
         
         assertEquals(WelcomeMsg, "Thank you for registering with Main Website Store.");
         
+		
+	}
+	
+	@Test(priority = 2, enabled = true)
+	public void LogOut () {
+		
+		//interact with web site
+		driver.get("https://magento.softwaretestingboard.com/customer/account/logout/");
+		// test web site
+		// لانو ال log out رجعني ع ال home page مشان هيك عملت من خلال الموقع
+		System.out.println(driver.getCurrentUrl());
+		//contains بترجع صح او غلط 
+		assertEquals(driver.getCurrentUrl().contains("logoutSuccess"), true);
+		
+	}
+	
+	@Test( priority = 3, enabled = true)
+	public void LogInProcess () throws InterruptedException {
+		// لما تشوف هاي الكلمة انقر 
+	   driver.findElement(By.linkText("Sign In")).click();
+	   driver.findElement(By.id("email")).sendKeys(Emailid);
+	   driver.findElement(By.id("pass")).sendKeys(CommonPassword);
+		
+		  driver.findElement(By.id("send2")).click();
+		
+		  driver.findElement(By.cssSelector(".greet.welcome")).getText();
+		  
+		     
+		  System.out.println(driver.findElement(By.cssSelector(".greet.welcome")).getText());
+		
+		  Thread.sleep(2000);
+		 String WelcomeMessage = driver.findElement(By.cssSelector(".greet.welcome")).getText();
+		  
+		 assertEquals(WelcomeMessage.contains("Welcome"),true);
+		
+		
+	}
+	
+	@Test( priority = 4, enabled = true)
+	public void addOneRandomItem () {
+		
+		driver.get("https://magento.softwaretestingboard.com/");
+		// انا هيك طلعت عدد التاغ a بس بنهاية الصفحة footer                    name class had
+		//WebElement footer = driver.findElement(By.cssSelector(".footer.content"));
+		//int theNumberOfTagA = footer.findElements(By.tagName("a")).size();
+		//System.out.println(theNumberOfTagA);
+		//بدي اطلع عدد ال h1 بكل الصفحة 
+		//int theNumberOfH1Tag = driver.findElements(By.tagName("h1")).size();
+		//System.out.println(theNumberOfH1Tag + "this is the total h1 tags inside the page ");
+		
+		WebElement ItemsContainer = driver.findElement(By.cssSelector(".product-items.widget-product-grid"));
+		int numberOfItems = ItemsContainer.findElements(By.tagName("li")).size();
+		System.out.println(numberOfItems);
+		
+		int randomItemToSelect =rand.nextInt(4); 
+		ItemsContainer.findElements(By.tagName("li")).get(randomItemToSelect).click();
+		
 		
 	}
 
